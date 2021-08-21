98ba895c6475a854d4cee21b7f1919b3885092a1
Ben Harder <ben.harder2@gmail.com>
2021-08-20 17:40:36 -0700

    lots of html updates

--- html/page.go
+++ html/page.go
@@ -10,16 +10,71 @@ import (
 
 func MainPage(memberNames []string, chores []choreManager.ChoreInterface, writer io.Writer) {
 	const tpl = `
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>{{.Title}}</title>
-		<h1>{{.Header}}<h1>
-	</head>
-	<body>
-		{{range .MemberNames}}<div>{{ . }}</div>{{else}}<div><strong>no rows</strong></div>{{end}}
-	</body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>{{.Title}}</title>
+  </head>
+  <body>
+  <div class="container">
+	<div class="row py-5">
+	  <div class="col">
+	  	<div class="container">
+		  <h1 class="display-4">Wonderful Estate</h1>
+		  <p class="lead">The official website of The Estate</p>
+		</div>
+	  </div>
+	</div>
+  	<div class="row">
+      <div class="col">
+    	<div class="container">
+		  <h1>Housemates</h1>
+		  {{range .MemberNames}}{{ . }}{{end}} 
+		</div>
+	  </div>
+	<div class="row py-4">
+	  <div class="col">
+	    <div class="container">
+		  <h1>Chores</h1>
+		  <table class="table">
+			<thead>
+				<tr>
+				<th scope="col">Schedule</th>
+				<th scope="col">Responsibilities</th>
+				<th scope="col">Date</th>
+				<th scope="col">Whose Turn</th>
+				</tr>
+			</thead>
+			<tbody>
+			{{range .Chores}}
+			  <tr>
+				<th scope="row">{{ .Schedule }}</th>
+				<td>{{ .Responsibilities }}</td>
+				<td>{{ .Date }}</td>
+				<td>{{ .WhoseTurn }}</td>
+			  </tr>
+			{{end}}
+			</tbody>
+			</table>
+	    </div>
+	  </div>
+	</div>
+  	</div>
+  </div>
+
+    
+
+	
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+  </body>
 </html>`
 
 	check := func(err error) {
@@ -34,10 +89,12 @@ func MainPage(memberNames []string, chores []choreManager.ChoreInterface, writer
 		Title       string
 		Header      string
 		MemberNames []string
+		Chores      []choreManager.ChoreInterface
 	}{
 		Title:       "Wonderful Estate",
 		Header:      "The Estate Chore Manager",
 		MemberNames: memberNames,
+		Chores:      chores,
 	}
 
 	err = t.Execute(writer, data)
