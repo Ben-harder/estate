e87d1b8f8a3af963dcae8c893c42660e4a56b827
Ben Harder <ben.harder2@gmail.com>
2021-09-19 10:29:15 -0700

    removing unused utility methods

--- household/household.go
+++ household/household.go
@@ -15,22 +15,13 @@ func NewHousehold(memberNames []string) (HouseholdInterface, error) {
 }
 
 type HouseholdInterface interface {
-	PrintHouseholdMembers()
 	Members() []MemberInterface
-	MemberNames() []string
 	String() string
-	MemberAfter(name string) (MemberInterface, error)
-	IsHouseholdMember(name string) bool
 	GetHouseholdMember(name string) (MemberInterface, error)
-	First() MemberInterface
 }
 
 type household struct {
 	members memberListInterface
-}
-
-func (hHold *household) PrintHouseholdMembers() {
-	fmt.Println(hHold.members.string())
 }
 
 // Members returns a copy of the household members
@@ -38,21 +29,6 @@ func (hHold *household) Members() []MemberInterface {
 	membersCopy := make([]MemberInterface, hHold.members.length())
 	copy(membersCopy, hHold.members.getMembers())
 	return membersCopy
-}
-
-// MemberNames returns the list of names of the members of the houeshold
-func (hHold *household) MemberNames() []string {
-	return hHold.members.memberNames()
-}
-
-// IsHouseholdMember returns true if the given name is one of the members of the household
-func (hHold *household) IsHouseholdMember(name string) bool {
-	_, err := hHold.members.getMember(name)
-	if err != nil {
-		return false
-	} else {
-		return true
-	}
 }
 
 // GetHouseholdMember returns the household member who has the given name or an error if they don't exist
@@ -68,21 +44,4 @@ func (hHold *household) GetHouseholdMember(name string) (MemberInterface, error)
 // String converts the memberlist to a comma delimited list of names
 func (hHold *household) String() string {
 	return hHold.members.string()
-}
-
-// Finds the next member of the household in alphabetical order
-func (hHold *household) MemberAfter(name string) (MemberInterface, error) {
-	mem, err := hHold.members.getMember(name)
-	if err != nil {
-		return nil, err
-	}
-	nextMember, err := hHold.members.next(mem)
-	if err != nil {
-		return nil, err
-	}
-	return nextMember, nil
-}
-
-func (hHold *household) First() MemberInterface {
-	return hHold.members.first()
 }
